e27175c72e8868ffd5162ebda5d667ad857d5021
mraiyuu <munenedenis618@gmail.com>
2024-01-29 22:31:20 +0300

    close button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { SiShopware } from "react-icons/si";
-import { mdOutlineCancle } from "react-icons/md";
+import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/dummy";
@@ -11,10 +11,18 @@ const Sidebar = () => {
   const activeMenu = true;
 
   return <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-hidden pb-10">
+
     { activeMenu && <div className="flex justify-between items-center">
       <Link to='/' onClick={() => {}}  className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
         <SiShopware  /> <span>Shoppy</span>
       </Link>
+      <TooltipComponent content="Menu" position="BottomCenter">
+        <button type="button" onClick={() => {}} 
+        className="text-xl p-3 rounded-full hover:bg-light-gray mt-4 block md:hidden"
+        >
+          <MdOutlineCancel />
+        </button>
+      </TooltipComponent>
       </div>}
   </div>;
 };
