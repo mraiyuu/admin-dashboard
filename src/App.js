f292e82ad0691d59556f1ca6bf1b96c5012537de
mraiyuu <munenedenis618@gmail.com>
2024-01-29 05:57:44 +0300

    active menu

--- src/App.js
+++ src/App.js
@@ -6,13 +6,30 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 
 const App = () => {
+
+
+  const activeMenu = true;
+
   return (
     <div>
       <BrowserRouter>
       <div className="flex relative dark:bg-main-dark-bg">
-        <div className="fixed right-4 bottom-4 style={{ zIndex: '10000' }}">
-          
+        <div className="fixed right-4 bottom-4 style={{ zIndex: '1000' }}">
+          <TooltipComponent content="settings" position="top">
+            <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{ background: 'blue', borderRadius: '50%'}}>
+              <FiSettings />
+            </button>
+          </TooltipComponent>
         </div>
+        {activeMenu ? (
+          <div>
+            sidebar
+          </div>
+        ) : (
+          <div>
+            sidebar w-0
+          </div>
+        )}
       </div>
       </BrowserRouter>
     </div>
