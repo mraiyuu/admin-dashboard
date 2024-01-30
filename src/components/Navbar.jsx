82857da76e3367bcde09efec0d1d9f235a7dd37d
mraiyuu <munenedenis618@gmail.com>
2024-01-30 23:35:21 +0300

    context navbar and sidebar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,33 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext();
+  const {
+    activeMenu,
+    setActiveMenu,
+    isClicked,
+    setIsClicked,
+    handleClick,
+    screenSize,
+    setScreenSize,
+  } = useStateContext();
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton
@@ -41,30 +67,35 @@ const Navbar = () => {
       <div className="flex">
         <NavButton
           title="Cart"
-          customFunc={() => handleClick('cart')}
+          customFunc={() => handleClick("cart")}
           color={"blue"}
           icon={<FiShoppingCart />}
         />
         <NavButton
           title="Chat"
           dotColor={"#03C9D7"}
-          customFunc={() => handleClick('chat')}
+          customFunc={() => handleClick("chat")}
           color={"blue"}
           icon={<BsChatLeft />}
         />
         <NavButton
           title="Notification"
           dotColor={"#03C9D7"}
-          customFunc={() => handleClick('notification')}
+          customFunc={() => handleClick("notification")}
           color={"blue"}
           icon={<RiNotification3Line />}
         />
         <TooltipComponent content="Profile" position="BottomCenter">
-          <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-full" onClick={() => handleClick('userProfile')}>
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-full"
+            onClick={() => handleClick("userProfile")}
+          >
             <img src={avatar} className="w-8 h-8 rounded-full" />
             <p>
-              <span className="text-14 text-gray-400">Hi, </span> { ' ' }
-              <span className="text-gray-400 text-14 ml-1 font-bold">Denis</span>
+              <span className="text-14 text-gray-400">Hi, </span>{" "}
+              <span className="text-gray-400 text-14 ml-1 font-bold">
+                Denis
+              </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
