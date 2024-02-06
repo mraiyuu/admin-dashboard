ce2c27373b71028a05a4ccf37f3d9577bf09e64d
mraiyuu <munenedenis618@gmail.com>
2024-02-06 06:34:29 +0300

    calender init

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import { ScheduleComponent, ViewsDirective, ViewDirective, Inject, Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule'
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars'
+
+import { scheduleData } from '../data/dummy'
+
+import { Header } from '../components'
+
 const Calender = () => {
   return (
-    <div>Calender</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Apps" title="Calendar" />
+      <ScheduleComponent>
+        <Inject services={[Day, Week, WorkWeek, Month, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+    </div>
   )
 }
 
