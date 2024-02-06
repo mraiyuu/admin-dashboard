432664e348799e0aad8bf0d0b41d092e3cde1904
mraiyuu <munenedenis618@gmail.com>
2024-02-06 23:38:43 +0300

    color picker refactor

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -3,6 +3,11 @@ import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
 
 import { Header } from "../components";
 
+const change = (args) => {
+  document.getElementById("preview").style.backgroundColor =
+    args.currentValue.hex;
+};
+
 const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
@@ -18,6 +23,7 @@ const ColorPicker = () => {
               modeSwitcher={false}
               inline
               showButtons={false}
+              change={change}
             />
           </div>
           <div>
@@ -28,6 +34,7 @@ const ColorPicker = () => {
               modeSwitcher={false}
               inline
               showButtons={false}
+              change={change}
             />
           </div>
         </div>
