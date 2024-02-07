f519aee7ab77616b5c9ae0775e5c11688761edc1
mraiyuu <munenedenis618@gmail.com>
2024-02-07 05:49:33 +0300

    theming

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { MdOutlineCancel } from 'react-icons/md'
+import { BsCheck } from 'react-icons/bs'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+
+import { themeColors } from '../data/dummy'
+
+import { useStateContext } from '../contexts/ContextProvider'
 
 const ThemeSetting = () => {
   return (
-    <div>ThemeSetting</div>
+    <div className='bg-half-transparent w-screen fixed right-0 top-0 nav-item'>
+      <div className='float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400'>
+        <div className='flex justify-between items-center p-4 ml-4'>
+          <p>Settings</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
